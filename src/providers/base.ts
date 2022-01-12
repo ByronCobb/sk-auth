90ebacf1ece50051be1ad6e3a8d4f7eb37348897
ByronCobb <byronrcobb@gmail.com>
2022-01-12 11:12:00 +0000

    revert

--- src/providers/base.ts
+++ src/providers/base.ts
@@ -32,11 +32,6 @@ export abstract class Provider<T extends ProviderConfig = ProviderConfig> {
     svelteKitAuth: Auth,
   ): EndpointOutput | Promise<EndpointOutput>;
 
-  abstract getProviderSigninUri<Locals extends Record<string, any> = Record<string, any>, Body = unknown>(
-    request: ServerRequest<Locals, Body>,
-    svelteKitAuth: Auth,
-  ): Promise<string>;
-
   abstract callback<Locals extends Record<string, any> = Record<string, any>, Body = unknown>(
     request: ServerRequest<Locals, Body>,
     svelteKitAuth: Auth,
